d6ffff892cd312adae625837bbdee53bd96a7764
Allbrhel <57718139+Allbrhel@users.noreply.github.com>
2019-11-15 15:41:57 +0100

    Update and rename semgej.m to semgejbarva.m

---
+++ semgejbarva.m
@@ -0,0 +1,32 @@
+clc, clear
+disp ('sem gej')
+
+%barva
+
+COM_CloseNXT all
+clc, clear, close;
+
+nxt = COM_OpenNXT();
+COM_SetDefaultNXT(nxt);
+OpenNXT2Color(SENSOR_4, 'FULL');
+while 1
+color = GetNXT2Color(SENSOR_4)
+
+motA= NXTMotor('A', 'Power',  0);
+
+   switch color
+       case 'BLACK'
+
+        motA.Power = 0;
+        motA.SendToNXT();
+       case 'WHITE'
+        motA.Power = 100;
+        motA.SendToNXT();
+       case 'RED'
+        motA.Power = -50;
+        motA.SendToNXT();
+           
+   end
+end
+    
+%konec barva
